ec2bee3b95bd5273d4c53a1612c082f371d78ed2
Sebastian Rivera <Sebitasrivera2698@gmail.com>
2019-09-30 21:51:32 -0400

    AR

---
+++ lab-ar/Assets/Ar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    public void DoOnPlane()
+    {
+        Debug.Log("Place Content");
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ lab-ar/Assets/Newbehaviourscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Newbehaviourscript : MonoBehaviour
+{
+
+
+    public void DoOnPlane()
+    {
+        Debug.Log("Place Content");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
